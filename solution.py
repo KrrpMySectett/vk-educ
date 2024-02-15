756270e01708bd0c3fc1b9b9fa5f6725e07819bb
KeepMySecret <158459827+KeepMySecret@users.noreply.github.com>
2024-02-15 13:23:12 +0300

    Модуль 1 задание 10

--- solution.py
+++ solution.py
@@ -1,6 +1,25 @@
-#coding: utf-8
-x = input()
-x = set(x)
-x = sorted(x)
-x = ' '.join(map(str,x))
-print(x)
+line = input()
+words = []
+current_word = ""
+for char in line:
+    if char.isalnum():
+        current_word += char
+    elif current_word:
+        words.append(current_word)
+        current_word = ""
+if current_word:
+    words.append(current_word)
+word_counts = {}
+for word in words:
+    word = word.lower()
+    if word in word_counts:
+        word_counts[word] += 1
+    else:
+        word_counts[word] = 1 
+most_common_word = ""
+most_common_count = 0
+for word, count in word_counts.items():
+    if count > most_common_count:
+        most_common_word = word
+        most_common_count = count
+print(most_common_word, most_common_count)
